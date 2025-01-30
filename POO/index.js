17e8e4afad6ac8576136b04c776c2148dc847714
franciscoruiz85 <francisco.ruizs2015@gmail.com>
2025-01-30 18:41:38 -0300

    Inicio desarrollo POO

---
+++ POO/index.js
@@ -0,0 +1,93 @@
+const quizzes = [
+    {
+        quizz: "Paises",
+        questions: [
+            {
+                question: "¿En qué país se baila la cueca?",
+                answers: ["Chile", "Italia", "Brasil", "Australia"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿En qué país se encuentra la torre de Pisa?",
+                answers: ["Italia", "Francia", "España", "Portugal"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿En qué país se encuentra la torre Eiffel?",
+                answers: ["Nueva Zelanda", "Argentina", "Egipto", "Francia"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿Qué país es conocido por sus estatuas de piedra?",
+                answers: ["China", "España", "India", "Japon"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿En qué país se encuentra la ciudad de Nueva York?",
+                answers: ["Estados Unidos", "Canadá", "China", "Australia"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿En qué país se baila la samba?",
+                answers: ["Brasil", "Australia", "Alemania", "Japon"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿En qué país habitaron los vikingos?",
+                answers: ["Noruega", "Italia", "Francia", "España"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿En qué país se encuentra el Coliseo?",
+                answers: ["Italia", "Francia", "España", "Japon"],
+                votes: [0, 0, 0, 0]
+            }
+        ]
+    },
+    {
+        quizz: "Música",
+        questions: [
+            {
+                question: "¿Quién era el vocalista del grupo Queen?",
+                answers: ["Michael Jackson", "Richard John", "Freddy Mercury", "Bob Dylan"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿Cuál es la canción más popular de Jay-Z?",
+                answers: ["99 Problems", "Smells Like Teen Spirit", "Young Forever", "Halo"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿Qué banda es conocida por su trabajo en el heavy metal?",
+                answers: ["Black Sabbath", "Led Zeppelin", "Metallica", "Iron Maiden"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿Cuál es el instrumento más vendido en la música clásica?",
+                answers: ["Violin", "Piano", "Saxophone", "Flute"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿Qué banda es conocida por su trabajo en el punk rock?",
+                answers: ["Ramones", "The Beatles", "The Rolling Stones", "The Who"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿Cuál es la canción más popular de The Beatles?",
+                answers: ["Help!", "Yellow Submarine", "Sgt. Pepper's Lonely Hearts Club Band", "Revolver"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿Quién canta la canción 'Thriller'?",
+                answers: ["Michael Jackson", "Elvis Presley", "Bob Dylan", "Prince"],
+                votes: [0, 0, 0, 0]
+            },
+            {
+                question: "¿Cuál es la canción más popular de Queen?",
+                answers: ["Bohemian Rhapsody", "A Night at the Opera", "Material Girl", "Don't Stop Me Now"],
+                votes: [0, 0, 0, 0]
+            }
+        ]
+    }
+];
+
